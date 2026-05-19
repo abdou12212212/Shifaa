153b99067ea81af27dc2fa85d89e068710d4b658
abdou12212212 <abderahmane475@gmail.com>
2026-05-19 15:38:36 +0200

    first commit data2

--- backend/index.js
+++ backend/index.js
@@ -45,6 +45,6 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 //Server
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://0.0.0.0:${PORT}`);
+app.listen(PORT,'0.0.0.0', () => {
+  console.log(`Server running on ${PORT}`);
 });
