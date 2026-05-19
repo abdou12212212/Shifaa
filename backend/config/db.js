d35e7954b168336d596ddefeca248b0ffb14ab7b
abdou12212212 <abderahmane475@gmail.com>
2026-05-19 13:58:32 +0200

    first commit data

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,6 +7,9 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true   // يتحقق من صحة شهادة SSL
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
